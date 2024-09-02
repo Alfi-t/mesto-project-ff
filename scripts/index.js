48c31a8e15097e5048495b2e1e4bd5bac566443a
Alfi-t <alfi.t@ya.ru>
2024-09-02 13:24:01 +0200

    Готовое задание

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,40 @@
-// @todo: Темплейт карточки
+// Темплейт карточки
+    const cardTemplate = document.querySelector('#card-template').content.querySelector('.card');
 
-// @todo: DOM узлы
+// Место, куда будут добавляться карточки
+    const placesList = document.querySelector('.places__list');
 
-// @todo: Функция создания карточки
+// Функция создания карточки
+    function createCard(cardData, deleteCard) {
 
-// @todo: Функция удаления карточки
+// Клонируем темплейт карточки
+    const cardElement = cardTemplate.cloneNode(true);
 
-// @todo: Вывести карточки на страницу
+// Находим элементы внутри карточки и устанавливаем им значения
+    cardElement.querySelector('.card__image').src = cardData.link;
+    cardElement.querySelector('.card__title').textContent = cardData.name;
+    cardElement.querySelector('.card__image').alt = cardData.name;
+
+// Находим кнопку удаления и добавляем ей обработчик клика
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', () => deleteCard(deleteButton));
+
+// Возвращаем созданный элемент карточки
+    return cardElement;
+}
+
+// Функция удаления карточки
+    function deleteCard(button) {
+    const card = button.closest('.card');
+    if (card) {
+        card.remove();
+    }
+}
+
+// Добавляем карточки на страницу
+    initialCards.forEach((cardData) => {
+    const cardElement = createCard(cardData, deleteCard);
+    placesList.appendChild(cardElement);
+});
+
+
